e558f01bfb930b324a76570eb6761c6b4242198c
Thanadon S <thanadon@gmail.com>
2023-08-29 13:38:20 +0700

    Update virusaction-test.sh

--- unit_tests/input/virusaction-test.sh
+++ unit_tests/input/virusaction-test.sh
@@ -3,7 +3,7 @@ if test ! `basename $CLAM_VIRUSEVENT_FILENAME` = "clam.exe"; then
 	echo "VirusEvent incorrect: $CLAM_VIRUSEVENT_FILENAME" >$1/test-clamd.log
 	exit 1
 fi
-if test ! "x$CLAM_VIRUSEVENT_VIRUSNAME" = "xClamAV-Test-File.UNOFFICIAL"; then
+if test ! "x$CLAM_VIRUSEVENT_VIRUSNAME" = "xClamAV-Test-File.THAIZONE"; then
 	echo "VirusName incorrect: $CLAM_VIRUSEVENT_VIRUSNAME" >$1/test-clamd.log
 	exit 2
 fi
